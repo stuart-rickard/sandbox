da68b678f053b712fb9933a20a2c240166c9a4fe
stuart-rickard <stuart@bau-dev.com>
2021-10-30 08:55:07 -0700

    a few tweaks

--- circles/javascript.js
+++ circles/javascript.js
@@ -1,37 +1,15 @@
-// set up position variables
-/*
-var xPosL2 = 150;
-let txPosL2 = xPosL2.toString() + "px";
-var yPosL2 = 75;
-let tyPosL2 = yPosL2.toString() + "px";
-var xPosL3 = 150;
-let txPosL3 = xPosL3.toString() + "px";
-var yPosL3 = 75;
-let tyPosL3 = yPosL3.toString() + "px";
-*/
-
-var thetaL2 = 0;
-// let tThetaL2 = thetaL2.toString();
-var thetaL3 = 0;
-// let tThetaL3 = thetaL3.toString();
-var thetaL4 = 0;
-
-/* document.getElementById("demo").innerHTML = "L2 x value is " + txPosL2 + 
-    " L2 y value is " + tyPosL2 +  
-    " L3 x value is " + txPosL3 + 
-    " L3 y value is " + tyPosL3 +
-    " L2 angle is " + tThetaL2 +
-    " L3 angle is " + tThetaL3;
-*/
+// set up angle variables
+var thetaL2 = 0.65;
+var thetaL3 = 1.7;
+var thetaL4 = 0.5;
 
 // use step button to update position variables
-// need to fix calculation to take account of the thicknes of the border
 document.getElementById("step").addEventListener("click", function(){
     
     // increment the angles
     thetaL2 = thetaL2 + 0.35;
     let tThetaL2 = thetaL2.toString();
-    thetaL3 = thetaL3 + 0.3;
+    thetaL3 = thetaL3 + 0.4;
     let tThetaL3 = thetaL3.toString();
     thetaL4 = thetaL4 + 0.25;
     let tThetaL4 = thetaL4.toString();
@@ -57,14 +35,5 @@ document.getElementById("step").addEventListener("click", function(){
     document.getElementById("layer-3").style.left = txPosL3;
     document.getElementById("layer-4").style.top = tyPosL4;
     document.getElementById("layer-4").style.left = txPosL4;
-
-/*    
-    document.getElementById("demo").innerHTML = "L2 x value is " + txPosL2 + 
-        " L2 y value is " + tyPosL2 +  
-        " L3 x value is " + txPosL3 + 
-        " L3 y value is " + tyPosL3 +
-        " L2 angle is " + tThetaL2 +
-        " L3 angle is " + tThetaL3;
-*/        
 
 });      
