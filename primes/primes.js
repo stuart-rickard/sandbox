9a8bca3c0fb2bc0c446931949aa288f4b8588ade
stuart-rickard <stuart@bau-dev.com>
2022-04-27 20:50:50 -0700

    primes algorithm refinements

--- primes/primes.js
+++ primes/primes.js
@@ -1,20 +1,11 @@
-// returns first prime greater than n; n is positive and less than 10^7
+// nextPrime returns first prime greater than n; n must be positive
+// set x at second to last line to use the nextPrime function
 
 const nextPrime = function (n) {
-  const limit = Math.pow(10, 7);
-  let numberToTest = 1;
   let primesArray = [];
 
   // isPrime function tests whether a number is prime, takes array of primes as an argument
   const isPrime = function (testNum, arrayOfPrimes) {
-    // check whether testNum is greater than the square of the largest prime; if so, there's a problem
-    let largestPrime = arrayOfPrimes[arrayOfPrimes.length - 1] || 1;
-    if (testNum > largestPrime ** 2) {
-      // if (testNum < Math.pow(largestPrime, 2)) {
-      console.log("testNum is too high in isPrime function");
-      return "error";
-    }
-
     // use the array to determine whether testNum is prime
     for (i = 0; i < arrayOfPrimes.length; i++) {
       // if the square of the current iteration of the prime array is greater than the testNum, testNum is prime, so return true (we are filtering out non-primes using code below this if statement)
@@ -23,23 +14,21 @@ const nextPrime = function (n) {
         return true;
       }
       // check whether testNum is divisible by the current iteration of the prime array, return false if it is
-      if (!testNum % arrayOfPrimes[i]) {
+      if (!(testNum % arrayOfPrimes[i])) {
         console.log(
           testNum + " is not prime; it is divisible by " + arrayOfPrimes[i]
         );
         return false;
       }
     }
+    // when we test n=2, arrayOfPrimes is empty, and we need to return true because 2 is prime
+    console.log(testNum + " is prime");
+    return true;
   };
 
   // function to create an array of primes in increasing order
   const createPrimesArray = function (array, hurdleNumber, testNum) {
-    if (testNum > hurdleNumber) {
-      console.log("testNum is too high in createPrimesArray function");
-      return "error";
-    }
     let hurdleExceeded = false;
-
     while (!hurdleExceeded) {
       if (isPrime(testNum, array)) {
         array.push(testNum);
@@ -54,7 +43,7 @@ const nextPrime = function (n) {
   };
 
   // create array of primes until the largest prime is greater than the square root of n
-  primesArray = createPrimesArray(primesArray, Math.sqrt(n), numberToTest);
+  primesArray = createPrimesArray(primesArray, Math.sqrt(n), 2);
   console.log(primesArray);
 
   // check numbers incrementally greater than n to determine whether they are prime
@@ -63,6 +52,7 @@ const nextPrime = function (n) {
     n++;
     // add another prime to array if n is greater than the square of the highest prime in the array
     if (n > primesArray[primesArray.length - 1] ** 2) {
+      console.log("adding another prime to the array");
       primesArray = createPrimesArray(
         primesArray,
         Math.sqrt(n),
@@ -76,4 +66,8 @@ const nextPrime = function (n) {
   }
 };
 
-console.log(nextPrime(15));
+// set x to the number you want to test; x must be an interger greater than or equal to 1
+let x = 3479;
+console.log(
+  "the lowest prime number that is greater than " + x + " is " + nextPrime(x)
+);
